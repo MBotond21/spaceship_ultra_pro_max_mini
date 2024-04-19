e1ec6c058d4f6dcd699f313e9ad4c3eb1c0334e1
Mészáros Botond <m.botond07@gmail.com>
2024-04-19 14:22:58 +0200

    cleanup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,19 +2,11 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home.tsx";
-import { NoPage } from "./pages/NoPage.tsx";
+import { Navi } from './components/Navi';
+
 function App() {
   return (<>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}>
-        <Route path="NoPage" element={<NoPage/>} />
-
-        </Route>
-      </Routes>
-    </BrowserRouter>      
-    </>)
+    <Navi />
+  </>)
 }
 export default App
