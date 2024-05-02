08b25127253719c0ba0f6633510a996616fc2750
P.Boti <botondpasztor@gmail.com>
2024-05-02 20:38:13 +0200

    Cards expand right (no animation)

--- frontend/src/components/Cards/index.tsx
+++ frontend/src/components/Cards/index.tsx
@@ -1,21 +1,28 @@
 import "./index.css";
+import { useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 
 interface Props{
   title: string,
   description: string,
-  img: string
+  img: string,
+
 }
 
 export function Cards(props: Props){
 
+    const [open, setOpen] = useState(false); 
+
     return (
-        <Card onClick={(event: React.MouseEvent<HTMLButtonElement>) => event.currentTarget.classList.contains("clicked")? event.currentTarget.classList.remove("clicked"): event.currentTarget.classList.add("clicked")}>
-          <Card.Img src={props.img}/>
-          <Card.Body>
+        <Card onClick={(event: React.MouseEvent<HTMLButtonElement>) => {setOpen(!open); event.currentTarget.classList.contains("clicked")? event.currentTarget.classList.remove("clicked"): event.currentTarget.classList.add("clicked");}}>
+          <div className="left">
+            <Card.Img src={props.img}/>
             <Card.Title>{props.title}</Card.Title>
-            <Card.Text>{props.description}</Card.Text>
-          </Card.Body>
+          </div> 
+          <div className="right">
+            {open && <Card.Text>{props.description}</Card.Text>}
+          </div> 
+          
         </Card>
       );
 }
--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,14 +1,20 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { Cards } from "../components/Cards";
+import { useState } from 'react';
 
 export function Main() {
+
     return (
         <>
             <Container>
                 <Row>
                     <Col sm={4}>
                         <Cards img="https://cdn.mos.cms.futurecdn.net/qTmvroik2NtHVGsfCHyyxF-650-80.jpg.webp" title="SpaceRocket" description="Lorem ipsun"/>
+                        <Cards img="https://cdn.mos.cms.futurecdn.net/qTmvroik2NtHVGsfCHyyxF-650-80.jpg.webp" title="SpaceRocket" description="Lorem ipsun"/>
+                        <Cards img="https://cdn.mos.cms.futurecdn.net/qTmvroik2NtHVGsfCHyyxF-650-80.jpg.webp" title="SpaceRocket" description="Lorem ipsun"/>
+                        <Cards img="https://cdn.mos.cms.futurecdn.net/qTmvroik2NtHVGsfCHyyxF-650-80.jpg.webp" title="SpaceRocket" description="Lorem ipsun"/>
                     </Col>
+                    
                 </Row>
             </Container>
         </>
