22b8d4b0139de9cbc68b8347592d64fdce599a91
Mészáros Botond <m.botond07@gmail.com>
2024-05-03 13:57:09 +0200

    rewriting card to popup after clicked

--- frontend/src/components/Cards/index.tsx
+++ frontend/src/components/Cards/index.tsx
@@ -1,29 +1,36 @@
 import "./index.css";
-import { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Card, Modal } from 'react-bootstrap';
 
-interface Props{
+interface Props {
   id: string,
   title: string,
   description: string,
   img: string,
-  collapse: () =>  void,
 }
 
-export function Cards(props: Props){
+export function Cards(props: Props) {
 
-    const [open, setOpen] = useState(false); 
+  const [open, setOpen] = useState(false);
 
-    return (
-        <Card id={props.id}  onClick={(event: React.MouseEvent<HTMLButtonElement>) => { props.collapse(); setOpen(!open); event.currentTarget.classList.contains("clicked")? event.currentTarget.classList.remove("clicked"): event.currentTarget.classList.add("clicked");}}>
-          <div className="left">
-            <Card.Img src={props.img}/>
-            <Card.Title>{props.title}</Card.Title>
-          </div> 
-          <div className="right">
-            {open && <Card.Text>{props.description}</Card.Text>}
-          </div> 
-          
-        </Card>
-      );
+  const handleOpen = () => { setOpen(true) }
+  const handleClose = () => { setOpen(false) }
+
+
+  return (
+    <>
+      <Card id={props.id} onClick={ handleOpen }>
+        <div className="left">
+          <Card.Img src={props.img} />
+          <Card.Title>{props.title}</Card.Title>
+        </div>
+      </Card>
+      <Modal show={open} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{props.description}</Modal.Body>
+      </Modal>
+    </>
+  );
 }
